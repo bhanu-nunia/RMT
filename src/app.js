65dd6704dd45a96afdadddb57eee9a5bbc9e7b97
bhanu_arya <bhanu.nunia@gmail.com>
2023-01-05 20:41:18 +0530

    cors enabled

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
 
 
 mongoose.set('strictQuery', true);
@@ -18,6 +19,7 @@ const routes = require('./routes/routes');
 const secureRoute = require('./routes/secure-routes');
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({
